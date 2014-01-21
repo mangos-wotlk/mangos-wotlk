4f85ff9d6c2d477359650c207b882c6f7d194465
Rochet2 <tqbattlenet@gmail.com>
2014-01-21 16:56:49 +0200

    Eluna Fix crash, add more security and fix function arguments

--- src/game/LuaEngine/GlobalMethods.h
+++ src/game/LuaEngine/GlobalMethods.h
@@ -988,7 +988,8 @@ namespace LuaGlobalFunctions
             if (end - start < 4) // no mandatory args, dont add
             {
                 while (end != start)
-                    lua_remove(L, --end);
+                    if (!lua_isnone(L, --end))
+                        lua_remove(L, end);
                 continue;
             }
 
@@ -1012,7 +1013,8 @@ namespace LuaGlobalFunctions
             nodes.push_back(*entry);
 
             while (end != start) // remove args
-                lua_remove(L, --end);
+                if (!lua_isnone(L, --end))
+                    lua_remove(L, end);
 
             lua_pop(L, 1);
         }
