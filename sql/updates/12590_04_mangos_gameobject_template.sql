170d2a5ce6011a97a5510fafdcedf43dd75d8f16
Salja <salja8840@gmail.com>
2014-02-12 14:28:11 +0100

    Eluna Revert SQL Updates for Eluna Change all back.
    NOTE:
      * for existing database you can use
    
    SQL:
      ALTER TABLE `game_weather` DROP COLUMN `ScriptName`;
      ALTER TABLE `quest_template` DROP COLUMN `MaxLevel`;
    
    Signed-off-by: Salja <salja8840@gmail.com>

--- sql/updates/12590_04_mangos_gameobject_template.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE db_version CHANGE COLUMN required_12579_03_mangos_quest_template required_12590_04_mangos_gameobject_template bit;
-
-ALTER TABLE `gameobject_template` DROP COLUMN `AIName`;
